ac8f8ace840afecee0e417fdbbd18c296df83411
cahos1159 <cahos1159@live.fr>
2019-06-13 11:27:50 +0200

    display basique

--- src/App.js
+++ src/App.js
@@ -1,26 +1,22 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Card, CardImg, CardText, CardBody,CardTitle, CardSubtitle, Button } from 'reactstrap';
+import {MOCK} from './Mock';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component {
+  render() {
+    return (
+     <Card>
+       <CardImg top width="100%" src={MOCK[0].picture} alt="Card image cap"></CardImg>
+         <CardBody>
+            <CardTitle>{MOCK[0].name}</CardTitle>
+            <CardText>{MOCK[0].description}</CardText>
+         </CardBody>
+       
+     </Card>
+    );
+  }
 }
-
 export default App;
---
+++ src/model/Ingredient.js
@@ -0,0 +1,17 @@
+class Ingredient extends Component {
+    constructor(props){
+        var id,name,quantity;
+        super(props);
+        
+
+    }
+    render() {
+      return (
+        <div className="Ingredients">
+          
+        </div>
+      );
+    }
+  }
+  export default App;
+  
